d64aa1b9372e10aa993dd4ef15292bc1c8a42ac4
Tim van der Slik Laptop <timvdslik@gmail.com>
2015-11-26 08:53:37 +0100

    Minor changes general cleanup of code

--- page-work.php
+++ page-work.php
@@ -42,9 +42,11 @@ get_header();
 <div id="page-work" class="container-fluid no-padding">
 
     <?php
-    $i = 1;
+    $i = 0;
+    $x = 1;
     $args = array('post_type' => 'portfolio');
     $the_query = new WP_Query($args);
+    $posts = $the_query->post_count;
     /*
     |----------------------------------------------------------------
     |   Check if the query returns any posts, if true render the
@@ -54,8 +56,17 @@ get_header();
     if($the_query->have_posts()) {
 
         while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
-            <div class="work-block col-md-4 no-padding" data-number="<?php echo $i; ?>">
+            <?php
+            /*
+            |----------------------------------------------------------------
+            |   Place an opening div for the row if the $i number is 0.
+            |----------------------------------------------------------------
+            */
+            if($i % 3 == 0){
+                echo '<div class="row no-margin work-row">';
+            }
+            ?>
+            <div class="work-block col-md-4 no-padding" data-number="<?php echo $x; ?>">
                 <div class="work-image">
                     <img src="<?php the_field('thumb'); ?>" alt="" width="100%"/>
 
@@ -78,17 +89,66 @@ get_header();
                     <a href="#" class="close-button"><i class="icon icon-cancel-1"></i><span class="bg"></span></a>
                 </div>
             </div>
+            <?
+            /*
+            |----------------------------------------------------------------
+            |   Increment the variables.
+            |----------------------------------------------------------------
+            */
+            $i++;
+            $x++;
 
-            <?php
-            if($i > 3){
-                $i = 1;
-            }
-            else {
-                $i++;
+            /*
+            |----------------------------------------------------------------
+            |   Place an closing div for the row if the $i number is 3.
+            |----------------------------------------------------------------
+            */
+            if($i % 3 == 0){
+                echo '</div>';
+                $x = 1;
             }
             ?>
 
         <?php endwhile;
+        /*
+        |----------------------------------------------------------------
+        |   If the posts in a row is not 3 add a empty project div.
+        |----------------------------------------------------------------
+        */
+        if($i == 5 || $i == 8 || $i == 11){
+            ?>
+            <div class="work-block-empty col-md-4 no-padding " data-number="<?php echo $x; ?>">
+                <div class="work-image">
+                    <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/empty-thumb.png' ?>" alt="" width="100%"/>
+                </div>
+            </div>
+            <?php
+        }
+        if($i == 4 || $i == 7 || $i == 10){
+            ?>
+            <div class="work-block-empty col-md-4 no-padding " data-number="<?php echo $x; ?>">
+                <div class="work-image">
+                    <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/empty-thumb.png' ?>" alt="" width="100%"/>
+                </div>
+            </div>
+            <div class="work-block-empty col-md-4 no-padding " data-number="<?php echo $x; ?>">
+                <div class="work-image">
+                    <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/empty-thumb.png' ?>" alt="" width="100%"/>
+                </div>
+            </div>
+        <?php
+        }
+
+
+        /*
+        |----------------------------------------------------------------
+        |   Place an closing div for the row if the loop is over and
+        |   are not a dividable by 3.
+        |----------------------------------------------------------------
+        */
+        if($i % 3 != 0){
+            echo '</div>';
+        }
     }
     else {
         echo 'Sorry no posts matched your criteria';
